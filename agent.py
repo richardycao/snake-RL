b33e826302549a54c88c331e4f190da6bf09c056
Richard Cao <richacao@amazon.com>
2020-12-09 00:31:18 -0800

    Fixed some bugs in the game engine. More changes to dqn.

--- agent.py
+++ agent.py
@@ -32,25 +32,27 @@ class DQN(nn.Module):
     def __init__(self, h, w, outputs):
         super().__init__()
         
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=2, stride=1)
-        self.bn1 = nn.BatchNorm2d(16, momentum=0.1)
+        self.bn0 = nn.BatchNorm2d(1)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=2, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=2, stride=1)
-        self.bn2 = nn.BatchNorm2d(32, momentum=0.1)
+        self.bn2 = nn.BatchNorm2d(32)
 
         # Calculates output size of convolutional operation
-        def conv2d_size_out(size, kernel_size=2, stride=1):
-            return (size - (kernel_size - 1) - 1) // stride + 1
-        convw = conv2d_size_out(conv2d_size_out(w))
-        convh = conv2d_size_out(conv2d_size_out(h))
+        def conv2d_size_out(size, kernel_size=2, stride=1, padding=0):
+            return (size - (kernel_size - 1) - 1 + 2*padding) // stride + 1
+        convw = conv2d_size_out(conv2d_size_out(w, padding=1))
+        convh = conv2d_size_out(conv2d_size_out(h, padding=1))
         linear_input_size = convw * convh * 32 # total number of input nodes for fully-connected layer
 
-        self.fc1 = nn.Linear(linear_input_size + 1, 64)
+        self.fc1 = nn.Linear(linear_input_size, 64)
         self.fc2 = nn.Linear(64, outputs)
 
-    def forward(self, x, direction):
+    def forward(self, x):
+        x = self.bn0(x)
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.fc1(torch.cat((x.view(x.size(0), -1), direction), dim=1)))
+        x = F.relu(self.fc1(x.view(x.size(0), -1)))
         x = self.fc2(x)
         return x
 
@@ -75,31 +77,20 @@ class SnakeAgent:
         self.BATCH_SIZE = 128
         self.GAMMA = 0.99
         self.EPS_START = 0.99
-        self.EPS_END = 0.05
-        self.EPS_DECAY = 1000
+        self.EPS_END = 0.01
+        self.EPS_DECAY = 3000
         self.TARGET_UPDATE = 10
         self.target_update_count = 0
 
         self.show_logs = False
 
     def action(self, state, t):
-        board, direction = state
-
-        # weights = self.policy_net(board, direction)
-        # if self.show_logs:
-        #     print(F.softmax(weights))
-        # return torch.multinomial(weights - torch.min(weights), 1)
-
-        ###### eps-greedy method ######
         sample = random.random()
         eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * np.exp(-t / self.EPS_DECAY)
 
         if sample > eps_threshold: # greedy
             with torch.no_grad():
-                # t.max(1) will return largest column value of each row.
-                # second column on max result is index of where max element was
-                # found, so we pick action with the larger expected reward.
-                return self.policy_net(board, direction).max(1)[1].view(1, 1)
+                return self.policy_net(state).max(1)[1].view(1, 1)
         else: # random
             return torch.tensor([[random.randrange(self.n_actions)]], device=self.device, dtype=torch.long)
 
@@ -107,35 +98,30 @@ class SnakeAgent:
         if len(self.memory) < self.BATCH_SIZE:
             return
         transitions = self.memory.sample(self.BATCH_SIZE)
-
-        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-        # Detailed explanation). This converts batch-array of Transitions to Transition of batch-arrays.
         batch = Transition(*zip(*transitions))
 
         # Creates a list (mask) of true/false corresponding to whether or not the next state is terminal      
         non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=self.device, dtype=torch.bool)
         if len([s[0] for s in batch.next_state if s is not None]) == 0:
             return
-        non_final_next_boards = torch.cat([s[0] for s in batch.next_state if s is not None])
-        non_final_next_directions = torch.cat([s[1] for s in batch.next_state if s is not None])
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
 
         # Get data batches
-        board_batch = torch.cat([s[0] for s in batch.state])
-        direction_batch = torch.cat([s[1] for s in batch.state])
+        states_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
 
-        state_action_values = self.policy_net(board_batch, direction_batch)
+        state_action_values = self.policy_net(states_batch).gather(1, action_batch)
 
         # state value or 0 in case the state was final.
         next_state_values = torch.zeros(self.BATCH_SIZE, device=self.device)
-        next_state_values[non_final_mask] = self.target_net(non_final_next_boards, non_final_next_directions).max(1)[0].detach()
+        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
 
         # Compute the expected Q values
         expected_state_action_values = (next_state_values * self.GAMMA).view(-1, 1) + reward_batch
 
         # Compute the loss
-        loss = F.mse_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+        loss = F.mse_loss(state_action_values, expected_state_action_values)
 
         # Optimize the model
         self.optimizer.zero_grad()
--- snake.py
+++ snake.py
@@ -34,8 +34,8 @@ class Snake(arcade.Window):
         self.right_margin = 20
         self.bot_margin = 20
         # Board params
-        self.rows = 6
-        self.cols = 6
+        self.rows = 4
+        self.cols = 4
         self.rect_width = (self.width - self.left_margin - self.right_margin) / self.cols
         self.rect_height = (self.height - self.top_margin - self.bot_margin) / self.rows
         self.line_width = 1
@@ -47,23 +47,24 @@ class Snake(arcade.Window):
         }
         # Snake params
         self.body = [(2,2), (2,1), (2,0)]
-        #self.direction = 2 # 0: left, 1: up, 2: right, 3: down
+        self.decay = 0.95
         # Food params
-        self.food_r = np.random.randint(0, high=self.rows)
-        self.food_c = np.random.randint(0, high=self.cols)
+        self.food = (0,0)
         #self.score = 0
-        self.avg_score = [0 for _ in range(30)]
+        self.avg_score = [0 for _ in range(100)]
         self.avg_score_count = 0
         # Others
         self.step_key = True
         self.speed = 1
         # AI params
-        #self.board = torch.zeros(self.rows, self.cols, dtype=torch.float32) # 0: empty, 1: snake, 2: food
-        #self.action = torch.full((1,1), 2) # 0: left, 1: up, 2: right, 3: down
-        #self.t = 0
         self.ai_enabled = False
 
         self.agent = SnakeAgent(self.cols, self.rows, 4)
+
+    def set_board(self, r, c, val):
+        self.board[self.rows - r - 1, c] = val
+    def set_food(self, rc):
+        self.food = (self.rows - rc[0] - 1, rc[1])
 
     def reset(self):
         # Erase the board (for AI)
@@ -72,23 +73,26 @@ class Snake(arcade.Window):
         self.t = 0
         self.score = 0
         # Erase old food
-        self.fill_rect(self.food_r, self.food_c, self.colors["EMPTY"])
+        self.fill_rect(self.food[0], self.food[1], self.colors["EMPTY"])
         # Erase old snake body
         for r, c in self.body:
             self.fill_rect(r, c, self.colors["EMPTY"])
 
         self.body = [(2,2), (2,1), (2,0)]
-        self.direction = np.random.randint(0, high=4)
+        self.direction = np.random.randint(0, high=4) if self.ai_enabled else 2
         # Fill in snake body
         for r, c in self.body:
-            self.board[r, c] = 1
+            self.set_board(r, c, 0.5)
             self.fill_rect(r, c, self.colors["BODY"])
+        self.set_board(self.body[0][0], self.body[0][1], 1)
         self.new_food()
 
     # Doesn't do anything atm
     def new_body(self):
-        head = np.array(self.body[0])
-        neck = np.array(self.body[1])
+        head = np.array([np.random.choice(self.rows), np.random.choice(self.cols)])
+        neck = head + np.array([0, np.random.choice(3)-1])
+        while not self.check_bounds(neck[0], neck[1]):
+            neck = head + np.array([0, np.random.choice(3)-1])
         hn = head - neck
         if hn[0] == 0 and hn[1] < 0:
             self.direction = 0
@@ -98,25 +102,21 @@ class Snake(arcade.Window):
             self.direction = 2
         elif hn[0] < 0 and hn[1] == 0:
             self.direction = 3
+
+        return [head, neck]
         
     def new_food(self):
-        # TODO
-        # if board has no empty spots:
-        #     self.reset()
-
-        # Erase food from board
-        self.board[self.food_r, self.food_c] = 0
+        empty_spots = (self.board == 0).nonzero()
+        if len(empty_spots) == 0: # if there are no empty spots
+            return
 
         # Make new food
-        self.food_r = np.random.randint(0, high=self.rows)
-        self.food_c = np.random.randint(0, high=self.cols)
-        if (self.food_r, self.food_c) in self.body:
-            self.new_food()
+        empty_idx = np.random.choice(np.arange(len(empty_spots)))
+        self.set_food(empty_spots[empty_idx])
         
         # Add food to board
-        self.board[self.food_r, self.food_c] = 2
-        # Draw food
-        self.fill_rect(self.food_r, self.food_c, self.colors["FOOD"])
+        self.set_board(self.food[0], self.food[1], 2)
+        self.fill_rect(self.food[0], self.food[1], self.colors["FOOD"])
         
     def setup(self):
         self.set_update_rate(1/self.speed) # n Hz for value 1/n
@@ -130,27 +130,18 @@ class Snake(arcade.Window):
                 self.draw_rect(r, c, self.colors["OUTLINE"])
 
     def on_draw(self):
-        # Fill in snake head
-        self.fill_rect(self.body[0][0], self.body[0][1], self.colors["BODY"])
+        pass
     
     def update(self, delta_time):
         ##########################################################################
-        if self.ai_enabled:                                                     ##
-            state = (self.board.view(1, 1, self.board.size()[0], self.board.size()[1]), 
-                     torch.tensor(self.direction, dtype=torch.float32).clone().detach().view(1, 1))                                ##
+        state = self.board.view(1, 1, self.board.size()[0], self.board.size()[1])
+        if self.ai_enabled:
             self.action = self.agent.action(state, self.t)                      ##
         ##########################################################################
 
         self.step_key = True
-
         r, c = self.body[0]
 
-        # if ate food
-        ate = False
-        if r == self.food_r and c == self.food_c:
-            ate = True
-            self.score += 1
-            self.new_food()
         # move the snake forward
         new_head = (r,c)
         # set movement direction
@@ -171,38 +162,48 @@ class Snake(arcade.Window):
             new_head = (r,c+1)
         elif self.direction == 3:
             new_head = (r-1,c)
+
         # if going to be (out of bounds) or (ate itself), then died
         died = False
-        if not self.check_bounds(new_head[0], new_head[1]) or (new_head[0], new_head[1]) in self.body[1:]:
+        if not self.check_bounds(new_head[0], new_head[1]) or (new_head[0], new_head[1]) in self.body[1:-1]:
             died = True
+        # if ate food
+        ate = False
+        if new_head[0] == self.food[0] and new_head[1] == self.food[1]:
+            ate = True
+            self.score += 1
 
         if not died:
+            # Add new head
+            self.set_board(r, c, 0.5)
             self.body.insert(0, new_head)
-            self.board[new_head[0], new_head[1]] = 1
+            self.set_board(new_head[0], new_head[1], 1)
 
-            # Erase the tail of the snake
-            if not ate:
+            # If not eaten, erase the tail of the snake
+            if ate:
+                self.new_food()
+            else:
                 self.fill_rect(self.body[-1][0], self.body[-1][1], self.colors["EMPTY"])
-                self.board[self.body[-1][0], self.body[-1][1]] = 0
+                self.set_board(self.body[-1][0], self.body[-1][1], 0)
                 self.body.pop(len(self.body) - 1)
+            self.fill_rect(new_head[0], new_head[1], self.colors["BODY"])
+                
             self.t += 1
         else:
-            #print("final score:", self.score)
             self.avg_score.append(self.score)
             self.avg_score.pop(0)
-            self.avg_score_count = (self.avg_score_count + 1) % 30
-            if self.avg_score_count == 0:
-                print("Average score:", np.average(self.avg_score))
+            self.avg_score_count +=1
+            if self.avg_score_count%len(self.avg_score) == 0:
+                print(str(self.avg_score_count) + ":", np.average(self.avg_score))
             self.reset()
 
         ##########################################################################
-        if self.ai_enabled:                                                     ##
-            reward = torch.tensor(-1 if died else (1 if ate else -0.05).view(1, 1))
-            next_state = (self.board.view(1, 1, self.board.size()[0], self.board.size()[1]), 
-                            torch.tensor(self.direction, dtype=torch.float32).clone().detach().view(1, 1)) if not died else None
-            self.agent.get_memory().push(state, self.action, next_state, reward)##
+        #if self.ai_enabled:                                                    ##
+        reward = torch.tensor(-1 if died else 1 if ate else 0).view(1, 1)
+        next_state = self.board.view(1, 1, self.board.size()[0], self.board.size()[1]) if not died else None
+        self.agent.get_memory().push(state, self.action, next_state, reward)    ##
                                                                                 ##
-            self.agent.optimize()                                               ##
+        self.agent.optimize()                                                   ##
         ##########################################################################
     
     def on_key_press(self, key, modifiers):
@@ -228,7 +229,7 @@ class Snake(arcade.Window):
             self.speed = 2
             self.set_update_rate(1/self.speed)
         elif key == arcade.key.W:
-            self.speed = 10
+            self.speed = self.rows
             self.set_update_rate(1/self.speed)
         elif key == arcade.key.E:
             self.speed = 200
